46aac78e7cc4001adbbb97a153f2146e661b0e9a
Eulier González <eulier1@gmail.com>
2025-02-04 12:19:51 -0300

    merging 2 streams

--- applications/merging-timelines/script.js
+++ applications/merging-timelines/script.js
@@ -1,4 +1,12 @@
-import { fromEvent, merge, interval, concat, race, forkJoin } from 'rxjs';
+import {
+  fromEvent,
+  merge,
+  interval,
+  concat,
+  race,
+  forkJoin,
+  first,
+} from 'rxjs';
 import { mapTo, startWith, take, map } from 'rxjs/operators';
 import {
   labelWith,
@@ -7,3 +15,16 @@ import {
   setStatus,
   bootstrap,
 } from './utilities';
+
+const start$ = fromEvent(startButton, 'click').pipe(mapTo(true));
+const pause$ = fromEvent(pauseButton, 'click').pipe(mapTo(false));
+
+const isRunning = merge(start$, pause$).pipe(startWith(false));
+
+const first$ = interval(1000).pipe(map(labelWith('First')), take(4));
+const second$ = interval(1000).pipe(map(labelWith('Second')), take(4));
+const combined$ = merge(first$, second$);
+
+isRunning.subscribe(setStatus);
+
+bootstrap({ first$, second$, combined$ });
